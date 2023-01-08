13218371203a1ae5a6903308efd22656bc9f3551
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-01-08 00:18:36 -0300

    perf/ modal layout, example modal whit buttonMd component, reac-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaBeer } from "react-icons/fa";
+import ButtonMd from "./components/buttons/button-md/ButtonMd";
+import { useState } from "react";
+import ModalLayout from "./components/modals/ModalLaout/ModalLayout";
 
 function App() {
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App text-center">
+      <h1 className="mb-8 flex flex-col items-center justify-center bg-primary_50 font-bold text-red-500">
+        REact Icon
+        <FaBeer className="text-center text-9xl" />
+      </h1>
+      <ButtonMd text="Log In" onClick={handleClick} lg />
+      {showModal && (
+        <ModalLayout
+          title={"Petagram"}
+          subtitle={"estrenando nuevo modal para petagram"}
+          textButton="Aceptar"
+          handleClick={handleClick}
+          handleClickClose={closeModal}
         >
-          Learn React
-        </a>
-      </header>
+          hola como estas
+        </ModalLayout>
+      )}
     </div>
   );
 }
---
+++ src/components/modals/ModalLaout/ModalLayout.jsx
@@ -0,0 +1,43 @@
+import ButtonMd from "../../buttons/button-md/ButtonMd";
+
+export default function ModalLayout({
+  handleClickClose,
+  handleClick,
+  title,
+  children,
+  textButton,
+  subtitle,
+}) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.3)]">
+      <div className="relative flex h-[500px] w-[450px] flex-col items-center justify-between rounded-lg bg-white p-4">
+        <div className="absolute top-2 right-2">
+          <button onClick={handleClickClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-7 w-7 cursor-pointer rounded-full bg-red-500 text-white transition-all hover:h-8 hover:w-8"
+            >
+              <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+            </svg>
+          </button>
+        </div>
+        <div className="flex flex-col gap-2 py-2 text-center">
+          <h1 className="text-3xl font-bold text-primary_500">{title}</h1>
+          <p className="text-xl ">{subtitle}</p>
+        </div>
+
+        <main className="flex h-3/5 w-5/6 items-center justify-center rounded-xl">
+          {children}
+        </main>
+
+        <div className="flex w-full justify-around p-6 text-white">
+          <ButtonMd danger text={"Cancelar"} md onClick={handleClickClose} md />
+
+          <ButtonMd md onClick={handleClick} text={textButton} />
+        </div>
+      </div>
+    </div>
+  );
+}
