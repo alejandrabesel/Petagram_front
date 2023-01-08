05eedafede84abdb9d937ffba9bd7335e1ec8f43
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-01-07 22:59:51 -0300

    perf/add size button md: el componente recibe una de dos propiedades md o lg, al pasar una de estas propiedades el ancho del boton difiere

--- src/components/buttons/button-md/ButtonMd.jsx
+++ src/components/buttons/button-md/ButtonMd.jsx
@@ -1,4 +1,12 @@
-export default function ButtonMd({ onClick, text, danger }) {
+import { useEffect } from "react";
+
+export default function ButtonMd({ onClick, text, danger, md, lg }) {
+  useEffect(() => {
+    if (!md && !lg)
+      throw new Error("not found size md or lg <ButtonMd md .../>");
+    if (md && lg)
+      throw new Error("you can't select both properties md && lg, choose one ");
+  }, []);
   return (
     <>
       <button
@@ -6,8 +14,8 @@ export default function ButtonMd({ onClick, text, danger }) {
           danger
             ? "bg-red-500 hover:bg-red-400"
             : "bg-primary_500 hover:bg-primary_400"
-        } text-md h-10 w-24 rounded-2xl
-        font-bold text-white shadow-md shadow-primary_800`}
+        } ${md && " w-24"}  ${lg && "w-44"} text-md
+        h-10 rounded-2xl font-bold text-white shadow-md shadow-primary_800`}
         onClick={onClick}
       >
         {text}
