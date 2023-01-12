3a8be69885e582c32026b244a3b4e868989ee6a5
pameluve <pameluve@gmail.com>
2023-01-11 22:54:37 -0400

    redux postSlice and postActions

--- src/redux/slices/postSlice.js
+++ src/redux/slices/postSlice.js
@@ -4,17 +4,29 @@ export const postsSlice = createSlice({
   name: "posts",
   initialState: {
     posts: [],
+    petPosts: [],
     post: {},
+    reactions: [],
+    comments:[]
   },
   reducers: {
     getAllPosts: (state, action) => {
       state.posts = action.payload;
     },
     getPostById: (state, action) => {
+      state.petPosts = action.payload;
+    },
+    postPost: (state, action)=>{
       state.post = action.payload;
     },
+    reactions: (state, action)=>{
+      state.reactions = action.payload
+    },
+    comments: (state, action)=>{
+      state.comments = action.payload
+    }
   },
 });
 
-export const { getAllPosts, getPostById } = postsSlice.actions;
+export const { getAllPosts, getPostById, postPost, reactions,comments } = postsSlice.actions;
 export default postsSlice.reducer;
