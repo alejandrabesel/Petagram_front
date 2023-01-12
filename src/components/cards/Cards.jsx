78b1041a1edaa2935f76927209a279a9b2a2a23b
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-01-12 00:08:47 -0300

    perf/ se hace la conexion con la bd y se renderizan los posts en landing, implementacion con redux-toolkit exitosa

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -4,55 +4,18 @@ import { getPosts } from "../../redux/actions/postActions";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function () {
-  //const [cards, setCards] = useState([]);
-  const posts = useSelector((state) => state.posts);
+  const { posts } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
-  const cards = [
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/hMyT4CDXR.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/rkiByec47.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/1-7cgoZSh.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/26pHT3Qk7.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/BFRYBufpm.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/33mJ-V3RX.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/-HgpNnGXl.jpg",
-    },
-    {
-      title: "Titulo de Imagen",
-      url: "https://cdn2.thedogapi.com/images/dW5UucTIW.jpg",
-    },
-  ];
+
   useEffect(() => {
     dispatch(getPosts());
   }, []);
   return (
     <div className="grid  w-full grid-cols-1 justify-center gap-4 p-2  md:grid-cols-2 lg:grid-cols-3">
-      {cards.map((e, index) => (
-        <Card key={index} title={e.title} image={e.url} />
-      ))}
+      {posts.length !== 0 &&
+        posts.map((post) => (
+          <Card key={post.id} title={post.title} image={post.image} />
+        ))}
     </div>
   );
 }
--- src/redux/actions/postActions.js
+++ src/redux/actions/postActions.js
@@ -1,6 +1,12 @@
 import axios from "axios";
-import { getAllPosts, getPostById, postPost, reactions,comments } from "../slices/postSlice";
-const URL = "http://localhost:4000/";
+import {
+  getAllPosts,
+  getPostById,
+  postPost,
+  reactions,
+  comments,
+} from "../slices/postSlice";
+const URL = "http://localhost:3001/";
 
 // const URL = process.env.REACT_APP_URL_API;
 
@@ -12,53 +18,53 @@ const URL = "http://localhost:4000/";
 //     .catch((e) => console.log(e.message));
 // };
 
-export const getPosts = ()=>{
-  return async (dispatch)=>{
-      const json = await axios.get(`${URL}post`);
-      return dispatch(getAllPosts(json.data));
-  }
+export const getPosts = () => {
+  return async (dispatch) => {
+    const json = await axios.get(`${URL}post`);
+    return dispatch(getAllPosts(json.data));
+  };
 };
 
-export const getPostsByPetId = (petId)=>{
-  return async (dispatch)=>{
-      try {
-          const json = await axios.get(`${URL}post/${petId}`);
-          return dispatch (getPostById(json.data));
-      } catch (error){
-          console.log(error);
-      }
-  }
+export const getPostsByPetId = (petId) => {
+  return async (dispatch) => {
+    try {
+      const json = await axios.get(`${URL}post/${petId}`);
+      return dispatch(getPostById(json.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
-export const createPost = (petId, payload)=> async (dispatch)=>{
-  try{
-      console.log(payload)
-      await axios.post(`${URL}post/${petId}`, payload);
-      return dispatch (postPost());
-  }catch (error){
-      console.log (error)
-      return alert ("Post not created");
-  }
-};
-
-export const addReactions = (id, payload)=> async(dispatch)=>{
+export const createPost = (petId, payload) => async (dispatch) => {
   try {
-      console.log(payload)
-      await axios.put(`${URL}post/reaction/${id}`, payload);
-      return dispatch(reactions());
-  }catch(error){
-      console.log (error)
-      return alert("Error adding reactions")
+    console.log(payload);
+    await axios.post(`${URL}post/${petId}`, payload);
+    return dispatch(postPost());
+  } catch (error) {
+    console.log(error);
+    return alert("Post not created");
   }
 };
 
-export const addComment = (id, payload)=> async(dispatch)=>{
+export const addReactions = (id, payload) => async (dispatch) => {
   try {
-      console.log(payload)
-      await axios.put(`${URL}post/comment/${id}`, payload);
-      return dispatch(comments());
-  }catch(error){
-      console.log (error)
-      return alert("Error adding comment")
+    console.log(payload);
+    await axios.put(`${URL}post/reaction/${id}`, payload);
+    return dispatch(reactions());
+  } catch (error) {
+    console.log(error);
+    return alert("Error adding reactions");
+  }
+};
+
+export const addComment = (id, payload) => async (dispatch) => {
+  try {
+    console.log(payload);
+    await axios.put(`${URL}post/comment/${id}`, payload);
+    return dispatch(comments());
+  } catch (error) {
+    console.log(error);
+    return alert("Error adding comment");
   }
 };
